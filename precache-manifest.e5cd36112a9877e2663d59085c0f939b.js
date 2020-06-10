f33ff14f32236624dba7797d44b99698f1f1bcf6
Sourabh <hande.sourabh@gmail.com>
2020-06-10 14:03:09 +0530

    Updates

---
+++ precache-manifest.e5cd36112a9877e2663d59085c0f939b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd19cb7045ab0dbf8aff76e03e035fe9",
+    "url": "/GithubFinder/index.html"
+  },
+  {
+    "revision": "c0aedac1dc2645438f2c",
+    "url": "/GithubFinder/static/css/main.9a947eff.chunk.css"
+  },
+  {
+    "revision": "1c1e2955ac29dca193ff",
+    "url": "/GithubFinder/static/js/2.2e38abda.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/GithubFinder/static/js/2.2e38abda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0aedac1dc2645438f2c",
+    "url": "/GithubFinder/static/js/main.9a44333d.chunk.js"
+  },
+  {
+    "revision": "63ae1e0f16619ea73761",
+    "url": "/GithubFinder/static/js/runtime-main.7d85bf2f.js"
+  }
+]);
